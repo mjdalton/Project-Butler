ccba374d9f8e1146d8d8c9cfb3e78f8550bc2352
Maggie <mjdalton@iastate.edu>
2017-12-09 15:53:21 -0600

    Fixed Logout Error
    User should be able to log in and out now

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@
 ?>
 
 		<br><br><h2>Would you like to logout?</h2><br><br><br>
-		<center><form action="includes/logout.inc.php" method="POST">
+		<center><form action="includes/logout-inc.php" method="POST">
 			<button type="submit" name="submit" class="resumeBtn">Logout</button>
 		</form></center>
 		<br><br><br><br><br><br><br><br><br><br><br><br><br><br>

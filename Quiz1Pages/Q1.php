5a5387f730a0c06599f3cacdf424e148564e560b
Maggie <mjdalton@iastate.edu>
2017-12-10 16:41:54 -0600

    Score Updating
    Score updates in db if user is logged in.  If it doesn't work, line 4 of Q1.php is likely culprit.

--- Quiz1Pages/Q1.php
+++ Quiz1Pages/Q1.php
@@ -1,44 +1,37 @@
 <?php
 
-	include 'header.php';
-?>		
+	include_once 'header.php';
+	include_once '../includes/dbh-inc.php'; //connection to the database
+?>
+		
+		
 		<?php
 			if (!isset($_SESSION['u_id'])) {
 				echo "<h2>WARNING: Your data will not be saved unless you login</h2><br><br><br>";
 			}
 		?>
 		<br><br><br>
-		<center><img src="../images/quiz1q1.png" alt="Question 1" class="img"><br><br><br><br><br>
-		<p>Input your answer here:</p>
-		<input id="numb"><br><br>
-		<button type="button" onclick="ansValidate()" class="submitBtn">Submit</button><br><br><br><br><br><br><br>
-		<a href="../quiz1.php"><button class="submitBtn"><- All Questions</button></a>
-		<a href="Q2.php"><button class="submitBtn">Question 2 -></button></a>
+		<center>
+			<img src="../images/quiz1q1.png" alt="Question 1" class="img">		<br><br><br><br><br>
+			<p>Input your answer here:</p>
+			<form action="includes/quiz1q1-inc.php" method="POST">
+				<input type="number" name="answer" placeholder="Your Answer" class="textBox"><br><br>
+				<button type="submit" name="submit" class="submitBtn">Submit</button>		<br><br><br><br>
+			</form>
+			<p id="demo"></p>
+		</center>  <!-- What is this line for? -->
 
-		<p id="demo"></p></center>
 
-		<script>
-		function ansValidate() {
-			var x, text;
-
-			// Get the value of the input field with id="numb"
-			x = document.getElementById("numb").value;
-
-			// If x is Not a Number or less than one or greater than 10
-			if (isNaN(x) || x != 10) {
-				text = "Input incorrect";
-			} else {
-				text = "Input correct";
-				<?php
-					//$sql = "INSERT INTO users (quiz1, score) VALUES ('3', '10')"; //Hopefully inputs values for completion of the question into the database
-					//echo '<p>Succesful?</p>';
-				?>
-			}
-			document.getElementById("demo").innerHTML = text;
-		}
-		</script><br><br>
-
-		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><!-- BS to make the footer stay at the bottom until a better solution is found -->
+		
+		<br><br><br>
+		<center>
+			<a href="../quiz1.php"><button class="submitBtn"><- All Questions</button></a>
+			<a href="Q2.php"><button class="submitBtn">Question 2 -></button></a>
+		</center>
+		
+		
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			
 <?php
-	include_once '../footer.php';
+	include_once 'footer.php';
 ?>
---
+++ includes/quiz1q1-inc.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if (isset($_POST['submit'])){ //checks if the submit button has been clicked
+
+	include_once 'dbh-inc.php';
+	$answer = mysqli_escape_string($conn, $_POST['answer']);
+	$currentScore = $_SESSION['u_score']; //get current score
+	$currentid = $_SESSION['u_id']; //get current user's id
+	$correctAns = 10;
+
+	echo '<p>Submitted</p>';
+
+	if($answer == $correctAns){
+		header("Location: ../quiz1q1.php?submission=correct");
+		$currentScore += 10;
+		$sql = "UPDATE users SET score='$currentScore' quiz1='$currentScore' WHERE id='$currentid'"; //update score in db
+		mysqli_query($conn, $sql);  //send command to update
+		$_SESSION['u_score'] = $currentScore; //update current score
+		$_SESSION['u_score'] = $currentScore;
+
+	}
+	else{
+		header("Location: ../quiz1q1.php?submission=incorrect");
+		echo '<h3>That is incorrect!</h3>';
+	}
+	
+
+}
+else{
+	header("Location: ../quiz1q1.php?submission=error");
+	exit();
+}

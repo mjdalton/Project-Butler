38ac2425159a12e0fdab215d1b6a687010c7085c
janetvc5 <janetv@iastate.edu>
2017-12-10 12:54:05 -0600

    final

---
+++ Exam1Pages/Q1.php
@@ -0,0 +1,16 @@
+<?php
+
+	include 'header.php';
+?>		
+		<h2>WARNING: Your data will not be saved unless you login</h2><br>
+		<h3>Insert quiz content here</h3><br>
+		<p>I'm thinking of also adding a drop down menu from the top menu bar that includes a list of links to all quizzes<br>The footer should stick to the bottom after we 
+			add more content.  I added another lighter color because all the colors we initially picked when put together were too dark.  Stuff blended into each other.  We can definitely
+			pick different colors, just maybe similar shades of light/dark to this?<br><br>For ease of work I've only done some of the changes to the quiz 1 page and we can copy it to the 
+			other pages if we like them.</p><br>
+			
+
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><!-- BS to make the footer stay at the bottom until a better solution is found -->
+<?php
+	include_once '../footer.php';
+?>
---
+++ exam2.php
@@ -0,0 +1,27 @@
+<?php
+
+	include_once 'header.php';
+	include_once 'includes/dbh-inc.php'; //connection to the database
+?>
+
+		<h2>WARNING: <br>Your data will not be saved unless you login</h2><br><br><br>
+
+		<center>
+			<h3>Question 1</h3><br>
+			<a href = "Exam2Pages/Q1.php"><img src="Exam2/Q1.png" alt="Question 1" class="imgLink"></a><br><br><br><br>
+			<h3>Question 2</h3><br>
+			<a href = "Exam2Pages/Q2.php"><img src="Exam2/Q2.png" alt="Question 2" class="imgLink"></a><br><br><br><br>
+			<h3>Question 3</h3><br>
+			<a href = "Exam2Pages/Q3.php"><img src="Exam2/Q3.png" alt="Question 3" class="imgLink"></a><br><br><br><br>
+			<h3>Question 4</h3><br>
+			<a href = "Exam2Pages/Q4.php"><img src="Exam2/Q4.png" alt="Question 4" class="imgLink"></a><br><br><br><br>
+			<h3>Question 5</h3><br>
+			<a href = "Exam2Pages/Q5.php"><img src="Exam2/Q5.png" alt="Question 5" class="imgLink"></a><br><br><br><br>
+		</center>
+		
+		
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			
+<?php
+	include_once 'footer.php';
+?>
